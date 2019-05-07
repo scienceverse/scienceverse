985b506c89729e02671293aab6eb32f56aaac729
debruine <debruine@gmail.com>
2019-05-07 09:56:31 +0100

    tests for custom analysis

--- tests/testthat/test-add_analysis.R
+++ tests/testthat/test-add_analysis.R
@@ -22,3 +22,27 @@ test_that("set values", {
   expect_equal(s$analyses[[1]]$params, params)
   expect_equal(s$analyses[[1]]$code, code)
 })
+
+# custom ----
+test_that("custom", {
+  mean_abs_diff <- function(x, y) {
+    (x - y) %>%
+      abs() %>%
+      mean() %>%
+      magrittr::set_names("mean_abs_diff") %>%
+      as.list()
+  }
+
+  s <- study() %>%
+    add_hypothesis() %>%
+    add_analysis("mean_abs_diff", list(
+      x = ".data[1]$Petal.Width",
+      y = ".data[1]$Petal.Length"
+    ), mean_abs_diff)
+
+  expect_equal(s$analyses[[1]]$code, mean_abs_diff)
+  expect_equal(s$analyses[[1]]$func, "mean_abs_diff")
+  expect_equal(s$analyses[[1]]$params %>% names(), c("x", "y"))
+  expect_equal(s$analyses[[1]]$params$x, ".data[1]$Petal.Width")
+  expect_equal(s$analyses[[1]]$params$y, ".data[1]$Petal.Length")
+})
