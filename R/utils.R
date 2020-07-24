b7590376bdc93e6bf9491234d784652120e37cb8
debruine <debruine@gmail.com>
2020-07-24 12:09:21 +0100

    Less scary green messages in RStudio

--- R/utils.R
+++ R/utils.R
@@ -326,3 +326,22 @@ print.scivrs_study <- function(x, ...) {
 print.scivrs_results <- function(x, ...) {
   cat(faux::nested_list(x))
 }
+
+#' Less scary green messages
+#'
+#' @param ... message components (see \code{\link[base]{message}})
+#' @param domain (see \code{\link[base]{message}})
+#' @param appendLF append new line? (see \code{\link[base]{message}})
+#'
+#' @return TRUE
+#' @keywords internal
+#'
+message <- function (..., domain = NULL, appendLF = TRUE) {
+  if (is.null(knitr::opts_knit$get('rmarkdown.pandoc.to'))) {
+    # not in knitr environment
+    base::message("\033[32m", ..., "\033[39m",
+                  domain = domain, appendLF = appendLF)
+  } else {
+    base::message(..., domain = domain, appendLF = appendLF)
+  }
+}
